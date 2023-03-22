6d62f77a5b21d4cd4adbfced9886abfd96f18d37
fslon <fedordikyn@gmail.com>
2023-03-22 18:02:00 +0300

    1 урок. 1 часть

--- app/src/main/java/ru/geekbrains/materialdesignapp/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/materialdesignapp/MainActivity.kt
@@ -2,10 +2,18 @@ package ru.geekbrains.materialdesignapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.geekbrains.materialdesignapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, PictureOfTheDayFragment.newInstance())
+                .commitNow()
+        }
     }
 }
---
+++ app/src/main/java/ru/geekbrains/materialdesignapp/PictureOfTheDayData.kt
@@ -0,0 +1,8 @@
+package ru.geekbrains.materialdesignapp
+
+sealed class PictureOfTheDayData {
+    data class Success(val serverResponseData: PODServerResponseData) :
+        PictureOfTheDayData()
+    data class Error(val error: Throwable) : PictureOfTheDayData()
+    data class Loading(val progress: Int?) : PictureOfTheDayData()
+}
