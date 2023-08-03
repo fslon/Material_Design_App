6b8d200f8d9ea2f3e1dced32aef9c42e5de05715
fslon <fedordikyn@gmail.com>
2023-08-03 23:34:09 +0300

    5 урок. trajectory and shuffle animations + ObjectAnimator, View.animate, StateListAnimator

--- app/src/main/java/ru/geekbrains/materialdesignapp/view/AnimationsFragment.kt
+++ app/src/main/java/ru/geekbrains/materialdesignapp/view/AnimationsFragment.kt
@@ -1,22 +1,48 @@
 package ru.geekbrains.materialdesignapp.view
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
 import android.graphics.Rect
 import android.os.Bundle
 import android.transition.*
 import android.view.*
-import android.widget.FrameLayout
-import android.widget.ImageView
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.GravityCompat
+import androidx.core.view.ViewCompat
+import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import ru.geekbrains.materialdesignapp.R
 import ru.geekbrains.materialdesignapp.databinding.FragmentAnimationsBinding
 
 
 class AnimationsFragment : Fragment() {
+
+    private val rotation = "rotation" // for object animator
+    private val translationY = "translationY" // for object animator
+    private val rotationFrom = 0f // for object animator
+    private val rotationTo = 225f // for object animator
+    private val translationOneContainer = -130f // for object animator
+    private val translationTwoContainer = -250f // for object animator
+    private val translationPlusImageFrom = 0f // for object animator
+    private val translationPlusImageTo = -180f // for object animator
+    private val translationZero = 0f // for object animator
+    private var isExpanded = false
+
+    lateinit var containerObjectAnimator: FrameLayout
+    lateinit var transparentBackgroundObjectAnimator: FrameLayout
+    lateinit var optionOneContainerObjectAnimator: LinearLayout
+    lateinit var optionTwoContainerObjectAnimator: LinearLayout
+    lateinit var fabObjectAnimator: FloatingActionButton
+    lateinit var plusImageViewObjectAnimator: ImageView
+    lateinit var scrollViewObjectAnimator: NestedScrollView
+    lateinit var toolbarObjectAnimator: FrameLayout
+
+
     private var _binding: FragmentAnimationsBinding? = null
     private val binding get() = _binding!!
 
@@ -164,12 +190,28 @@ class AnimationsFragment : Fragment() {
                         ConstraintLayout.LayoutParams.MATCH_PARENT
                     )
 
-                    trajectoryMovementLayout.getViewById(R.id.trajectory_movement_button).setOnClickListener {
-                        TransitionManager.beginDelayedTransition(binding.animationsContainer)
-                        textIsVisible = !textIsVisible
-                        trajectoryMovementLayout.getViewById(R.id.trajectory_movement_text).visibility =
-                            if (textIsVisible) View.VISIBLE else View.GONE
+
+                    var toRightAnimation = false
+                    val containerTrajectory = trajectoryMovementLayout.getViewById(R.id.trajectory_movement_container) as ConstraintLayout
+                    val frameLayoutTrajectory = containerTrajectory.findViewById(R.id.trajectory_movement_frame_layout) as FrameLayout
+                    val buttonIncreaseLayout = frameLayoutTrajectory.findViewById<Button>(R.id.trajectory_movement_button)
+
+                    buttonIncreaseLayout.setOnClickListener {
+                        val changeBounds = ChangeBounds()
+                        changeBounds.setPathMotion(ArcMotion())
+                        changeBounds.duration = 500
+                        TransitionManager.beginDelayedTransition(
+                            binding.transitionsContainer,
+                            changeBounds
+                        )
+                        toRightAnimation = !toRightAnimation
+                        val params = buttonIncreaseLayout.layoutParams as FrameLayout.LayoutParams
+                        params.gravity =
+                            if (toRightAnimation) Gravity.END or Gravity.BOTTOM else Gravity.START or Gravity.TOP
+                        buttonIncreaseLayout.layoutParams = params
                     }
+
+
                 }
 
                 R.id.shuffle_objects -> {
@@ -185,55 +227,75 @@ class AnimationsFragment : Fragment() {
                         ConstraintLayout.LayoutParams.MATCH_PARENT
                     )
 
-                    transitionShuffleObjectsLayout.getViewById(R.id.shuffle_objects_button).setOnClickListener {
-                        TransitionManager.beginDelayedTransition(binding.animationsContainer)
-                        textIsVisible = !textIsVisible
-                        transitionShuffleObjectsLayout.getViewById(R.id.shuffle_objects_text).visibility =
-                            if (textIsVisible) View.VISIBLE else View.GONE
+                    val containerShuffle = transitionShuffleObjectsLayout.getViewById(R.id.transition_shuffle_objects_container) as ConstraintLayout
+                    val parentLinearLayoutShuffle =
+                        containerShuffle.findViewById(R.id.transition_shuffle_objects_linear_layout_parent) as LinearLayout
+                    val childLinearLayoutShuffle = containerShuffle.findViewById(R.id.transition_shuffle_objects_linear_layout_child) as LinearLayout
+                    val buttonShuffle = parentLinearLayoutShuffle.findViewById<Button>(R.id.transition_shuffle_objects_button)
+
+                    val titles: MutableList<String> = ArrayList()
+                    for (i in 0..4) {
+                        titles.add(String.format("Item %d", i + 1))
                     }
+                    createViews(childLinearLayoutShuffle, titles)
+                    buttonShuffle.setOnClickListener {
+                        TransitionManager.beginDelayedTransition(
+                            childLinearLayoutShuffle,
+                            ChangeBounds()
+                        )
+                        titles.shuffle()
+                        createViews(childLinearLayoutShuffle, titles)
+                    }
+
                 }
 
-                R.id.object_animator_for_drawer -> {
+                R.id.object_animator_for_drawer -> { // stateListAnimator добавлен сюда
                     navigationViewButtonsAction("object_animator")
 
                     binding.animationsContainer.removeAllViews()
 
-                    val objectAnimatorForDrawer: ConstraintLayout =
-                        layoutInflater.inflate(R.layout.transition_object_animator_layout, null) as ConstraintLayout
+                    val objectAnimatorLayout: FrameLayout =
+                        layoutInflater.inflate(R.layout.transition_object_animator_layout, null) as FrameLayout
                     binding.animationsContainer.addView(
-                        objectAnimatorForDrawer,
+                        objectAnimatorLayout,
                         ConstraintLayout.LayoutParams.MATCH_PARENT,
                         ConstraintLayout.LayoutParams.MATCH_PARENT
                     )
 
-                    objectAnimatorForDrawer.getViewById(R.id.object_animator_for_drawer_button).setOnClickListener {
-                        TransitionManager.beginDelayedTransition(binding.animationsContainer)
-                        textIsVisible = !textIsVisible
-                        objectAnimatorForDrawer.getViewById(R.id.object_animator_for_drawer_text).visibility =
-                            if (textIsVisible) View.VISIBLE else View.GONE
+                    containerObjectAnimator = objectAnimatorLayout.findViewById(R.id.object_animator_container) as FrameLayout
+                    transparentBackgroundObjectAnimator =
+                        containerObjectAnimator.findViewById(R.id.object_animator_transparent_background) as FrameLayout
+                    optionOneContainerObjectAnimator =
+                        containerObjectAnimator.findViewById(R.id.object_animator_option_one_container) as LinearLayout
+                    optionTwoContainerObjectAnimator =
+                        containerObjectAnimator.findViewById(R.id.object_animator_option_two_container) as LinearLayout
+                    fabObjectAnimator = containerObjectAnimator.findViewById(R.id.object_animator_fab) as FloatingActionButton
+                    plusImageViewObjectAnimator = containerObjectAnimator.findViewById(R.id.object_animator_plus_imageview) as ImageView
+                    scrollViewObjectAnimator = containerObjectAnimator.findViewById(R.id.object_animator_scroll_view) as NestedScrollView
+                    toolbarObjectAnimator = containerObjectAnimator.findViewById(R.id.object_animator_toolbar) as FrameLayout
+
+                    setFAB()
+
+                    scrollViewObjectAnimator.setOnScrollChangeListener { _, _, _, _, _ -> // появляется граница тулбара после скролла
+                        toolbarObjectAnimator.isSelected =
+                            scrollViewObjectAnimator.canScrollVertically(-1)
                     }
                 }
 
-                R.id.state_list_animator_for_drawer -> {
-                    navigationViewButtonsAction("state_list_animator_for_drawer")
-
-                    binding.animationsContainer.removeAllViews()
-
-                    val stateListAnimatorForDrawer: ConstraintLayout =
-                        layoutInflater.inflate(R.layout.transition_state_list_animator_layout, null) as ConstraintLayout
-                    binding.animationsContainer.addView(
-                        stateListAnimatorForDrawer,
-                        ConstraintLayout.LayoutParams.MATCH_PARENT,
-                        ConstraintLayout.LayoutParams.MATCH_PARENT
-                    )
-
-                    stateListAnimatorForDrawer.getViewById(R.id.state_list_animator_button).setOnClickListener {
-                        TransitionManager.beginDelayedTransition(binding.animationsContainer)
-                        textIsVisible = !textIsVisible
-                        stateListAnimatorForDrawer.getViewById(R.id.state_list_animator_text).visibility =
-                            if (textIsVisible) View.VISIBLE else View.GONE
-                    }
-                }
+//                R.id.state_list_animator_for_drawer -> {
+//                    navigationViewButtonsAction("state_list_animator_for_drawer")
+//
+//                    binding.animationsContainer.removeAllViews()
+//
+//                    val stateListAnimatorForDrawer: ConstraintLayout =
+//                        layoutInflater.inflate(R.layout.transition_state_list_animator_layout, null) as ConstraintLayout
+//                    binding.animationsContainer.addView(
+//                        stateListAnimatorForDrawer,
+//                        ConstraintLayout.LayoutParams.MATCH_PARENT,
+//                        ConstraintLayout.LayoutParams.MATCH_PARENT
+//                    )
+//
+//                }
 
                 R.id.constraint_layout_and_constraint_set -> {
                     navigationViewButtonsAction("constraint_layout_and_constraint_set")
@@ -306,9 +368,137 @@ class AnimationsFragment : Fragment() {
                 }
             })
 
-
         TransitionManager.beginDelayedTransition(recyclerViewExplodeLayout, set)
         recyclerViewExplodeLayout.adapter = null
+    }
+
+    private fun createViews(layout: ViewGroup, titles: List<String>) {
+        layout.removeAllViews()
+        for (title in titles) {
+            val textView = TextView(requireContext())
+            textView.text = title
+            textView.gravity = Gravity.CENTER_HORIZONTAL
+            ViewCompat.setTransitionName(textView, title)
+            layout.addView(textView)
+        }
+    }
+
+    private fun setFAB() {
+        setInitialState()
+        fabObjectAnimator.setOnClickListener {
+            if (isExpanded) {
+                collapseFab()
+            } else {
+                expandFAB()
+            }
+        }
+    }
+
+    private fun setInitialState() {
+        transparentBackgroundObjectAnimator.apply {
+            alpha = 0f
+        }
+        optionTwoContainerObjectAnimator.apply {
+            alpha = 0f
+            isClickable = false
+        }
+        optionOneContainerObjectAnimator.apply {
+            alpha = 0f
+            isClickable = false
+        }
+    }
+
+    private fun expandFAB() {
+        isExpanded = true
+        ObjectAnimator.ofFloat(
+            plusImageViewObjectAnimator, rotation, rotationFrom,
+            rotationTo
+        ).start()
+        ObjectAnimator.ofFloat(
+            optionTwoContainerObjectAnimator, translationY,
+            translationTwoContainer
+        ).start()
+        ObjectAnimator.ofFloat(
+            optionOneContainerObjectAnimator, translationY,
+            translationOneContainer
+        ).start()
+        optionTwoContainerObjectAnimator.animate()
+            .alpha(1f)
+            .setDuration(300)
+            .setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    optionTwoContainerObjectAnimator.isClickable = true
+                    optionTwoContainerObjectAnimator.setOnClickListener {
+                        Toast.makeText(
+                            requireActivity(), "Option 2",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            })
+        optionOneContainerObjectAnimator.animate()
+            .alpha(1f)
+            .setDuration(300)
+            .setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    optionOneContainerObjectAnimator.isClickable = true
+                    optionOneContainerObjectAnimator.setOnClickListener {
+                        Toast.makeText(
+                            requireActivity(), "Option 1",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            })
+        transparentBackgroundObjectAnimator.animate()
+            .alpha(0.9f)
+            .setDuration(300)
+            .setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    transparentBackgroundObjectAnimator.isClickable = true
+                }
+            })
+    }
+
+    private fun collapseFab() {
+        isExpanded = false
+        ObjectAnimator.ofFloat(
+            plusImageViewObjectAnimator, rotation,
+            translationPlusImageFrom, translationPlusImageTo
+        ).start()
+        ObjectAnimator.ofFloat(
+            optionTwoContainerObjectAnimator, translationY,
+            translationZero
+        ).start()
+        ObjectAnimator.ofFloat(
+            optionOneContainerObjectAnimator, translationY,
+            translationZero
+        ).start()
+        optionTwoContainerObjectAnimator.animate()
+            .alpha(0f)
+            .setDuration(300)
+            .setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    optionTwoContainerObjectAnimator.isClickable = false
+                    optionOneContainerObjectAnimator.setOnClickListener(null)
+                }
+            })
+        optionOneContainerObjectAnimator.animate()
+            .alpha(0f)
+            .setDuration(300)
+            .setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    optionOneContainerObjectAnimator.isClickable = false
+                }
+            })
+        transparentBackgroundObjectAnimator.animate()
+            .alpha(0f)
+            .setDuration(300)
+            .setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    transparentBackgroundObjectAnimator.isClickable = false
+                }
+            })
     }
 
 
@@ -330,6 +520,7 @@ class AnimationsFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
 
     companion object {
         fun newInstance() = AnimationsFragment()
